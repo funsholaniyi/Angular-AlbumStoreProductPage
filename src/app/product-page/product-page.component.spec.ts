f528ace1e9a6732858b34ad39fb6d840d5a064a3
Funsho Olaniyi <funsholaniyi@gmail.com>
2019-11-05 03:35:24 +0300

    Updated description

--- src/app/product-page/product-page.component.spec.ts
+++ src/app/product-page/product-page.component.spec.ts
@@ -8,9 +8,9 @@ describe('ProductPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductPageComponent ]
+      declarations: [ProductPageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
