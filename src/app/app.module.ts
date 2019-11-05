4c1dd81fe3d01f3922959b7a3e8db46d0bd32da8
Funsho Olaniyi <funsholaniyi@gmail.com>
2019-11-05 10:08:45 +0300

    Updated Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductService } from './product.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
